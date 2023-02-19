3b94fdb73bbad417b811f34181fd1f1e92ce1080
Nesrux <joaomarcosdevs@gmail.com>
2023-02-19 16:02:56 -0300

    Fazendos as coisas se mecherem

--- src/Jogo/FlapyBird.java
+++ src/Jogo/FlapyBird.java
@@ -3,6 +3,10 @@ package Jogo;
 import java.util.Set;
 
 public class FlapyBird implements Jogo {
+	public double chao_offset = 0;
+
+	// gvx
+	public double chao_vel = 50;
 
 	@Override
 	public String getTitulo() {
@@ -24,6 +28,8 @@ public class FlapyBird implements Jogo {
 
 	@Override
 	public void tique(Set<String> teclas, double dt) {
+		chao_offset += dt * chao_vel;
+		chao_offset = chao_offset % 112;
 
 	}
 
@@ -37,11 +43,12 @@ public class FlapyBird implements Jogo {
 		// Desenha o background do jogo
 		tela.imagem("flappy.png", 0, 0, 288, 512, 0, 0, 0);
 		tela.imagem("flappy.png", 0, 0, 288, 512, 0, 288, 0);
-		tela.imagem("flappy.png", 0, 0, 288, 512, 0, 288 * 2, 0);
+		// tela.imagem("flappy.png", 0, 0, 288, 512, 0, 288 * 2, 0);
 
 		// Desenha o chão do jogo
-		tela.imagem("flappy.png", 292, 0, 308, 112, 0, 0, 400);
-		tela.imagem("flappy.png", 292, 0, 308, 112, 0, 288, 400);
+		tela.imagem("flappy.png", 292, 0, 308, 112, 0, -chao_offset, getAltura() - 112);
+		tela.imagem("flappy.png", 292, 0, 308, 112, 0, 288, getAltura() - 112);
+		// tela.imagem("flappy.png", 292, 0, 308, 112, 0, 288 * 2, 400);
 	}
 
 	public static void main(String[] args) {
--- src/Jogo/Passaro.java
+++ src/Jogo/Passaro.java
@@ -2,9 +2,13 @@ package Jogo;
 
 public class Passaro {
 	public double x, y;
+
+	// = vy
 	public double velocidadeY;
-	
+
+	// = G
 	public static double GRAVIDADE = 500;
+
 	public static double FLAP = -300;
 
 }
