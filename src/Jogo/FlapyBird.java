0812e21424cbe00d2c7cb1ec441f5ffda7df63d0
Nesrux <joaomarcosdevs@gmail.com>
2023-02-19 15:29:05 -0300

    Criando classes e implementando a inteface Jogo

---
+++ src/Jogo/FlapyBird.java
@@ -0,0 +1,42 @@
+package Jogo;
+
+import java.util.Set;
+
+public class FlapyBird implements Jogo {
+
+	@Override
+	public String getTitulo() {
+		// TODO Auto-generated method stub
+		return "Flapy Bird";
+	}
+
+	@Override
+	public int getAltura() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLargura() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void tique(Set<String> teclas, double dt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tecla(String tecla) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void desenhar(Tela tela) {
+		// TODO Auto-generated method stub
+
+	}
+}
